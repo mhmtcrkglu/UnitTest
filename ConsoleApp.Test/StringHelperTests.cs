8d1e485a705e3301354a9e5662889f2b413ce48f
Mehmet Çürükoğlu <mehmetcurukoglu@hyundaiassan.com.tr>
2019-04-20 14:35:52 +0300

    first test scenario added and tested.

---
+++ ConsoleApp.Test/StringHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Test
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void Clear_Begin_End_Spaces()
+        {
+            //Arrange
+            var input = "        Happy  New Year    ";
+            var waitingResult = "Happy New Year";
+
+            //Act
+            var comingResult = StringHelper.ClearSpaces(input);
+
+            //Assert
+            Assert.AreEqual(waitingResult, comingResult);
+
+        }
+    }
+}
---
+++ ConsoleApp/StringHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+   public class StringHelper
+    {
+        // 1.case - Clear begin and end space
+        // 2.case - Clear repetitive space (more than one)
+        public static string ClearSpaces(string input)
+        {
+            input = input.Trim();
+
+            string newValue = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ' && input[i + 1] == ' ')
+                    continue;
+                newValue += input[i];
+            }
+
+            return newValue;
+        }
+    }
+}
