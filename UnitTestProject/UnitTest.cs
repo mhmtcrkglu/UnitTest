5a4e05e150436c02d6ff2ab214f66c46586431bf
Mehmet Çürükoğlu <mehmetcurukoglu@hyundaiassan.com.tr>
2019-04-20 14:23:05 +0300

    String Helper class created.

---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass] // for test class attribute
+    public class UnitTest
+    {
+        [TestMethod] // for test method attribute
+        public void TestMethod()
+        {  
+        }
+    }
+}
